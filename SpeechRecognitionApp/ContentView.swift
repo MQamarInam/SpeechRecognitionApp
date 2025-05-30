17986084d6092e33bb63130d61b02edcffc094e5
MQamarInam <qamarinam028@gmail.com>
2025-05-30 20:03:14 +0500

    Initial Commit

---
+++ SpeechRecognitionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechRecognitionApp
+//
+//  Created by Qaim's Macbook  on 30/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechRecognitionApp/SpeechRecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionAppApp.swift
+//  SpeechRecognitionApp
+//
+//  Created by Qaim's Macbook  on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
